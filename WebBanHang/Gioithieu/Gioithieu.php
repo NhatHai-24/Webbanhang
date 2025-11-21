ab54789be6acc12a09a8353ee6fa94c7b2b71128
Nguyễn vân thiên <thiennv0920@ut.edu.vn>
2025-11-21 15:52:28 +0700

    chinh phan giaodien

--- WebBanHang/Gioithieu/Gioithieu.php
+++ WebBanHang/Gioithieu/Gioithieu.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Về Fox Tech - Tương lai công nghệ</title>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="demo.css">
+    <link rel="stylesheet" href="Gioithieu.css">
 </head>
 <body>
 
